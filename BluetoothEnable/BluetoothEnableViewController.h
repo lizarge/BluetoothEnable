b764de5eafeb83a22fed675cae07118e8f20317c
Jeff McLeman <jeffmc@JeffMc-Office.local>
2011-04-04 11:28:37 -0700

    initial commit

--- BluetoothEnable/BluetoothEnableViewController.h
+++ BluetoothEnable/BluetoothEnableViewController.h
@@ -7,9 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+@class BluetoothManager;
 
 @interface BluetoothEnableViewController : UIViewController {
     
+    IBOutlet UILabel  *programLabel;
+    IBOutlet UIButton *enableButton;
+    IBOutlet UILabel  *copyrightLabel;
+    IBOutlet UILabel  *copyrightLabel2;
+    IBOutlet UILabel  *statusLabel;
+    id  bluetoothEnabled;
+    id btCont;
 }
+
+@property (nonatomic, retain) UILabel  *programLabel;
+@property (nonatomic, retain) UIButton *enableButton;
+@property (nonatomic, retain) UILabel  *copyrightLabel;
+@property (nonatomic, retain) UILabel  *copyrightLabel2;
+@property (nonatomic, assign) id bluetoothEnabled;
+@property (nonatomic, retain) id btCont;
+@property (nonatomic, retain) UILabel  *statusLabel;
+-(IBAction)handleButtonPress:(id)sender;
+-(void)showBTStatus;
 
 @end
--- BluetoothEnable/BluetoothEnableViewController.m
+++ BluetoothEnable/BluetoothEnableViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "BluetoothEnableViewController.h"
+@class BluetoothManager;
 
 @implementation BluetoothEnableViewController
+@synthesize programLabel;
+@synthesize enableButton;
+@synthesize copyrightLabel;
+@synthesize copyrightLabel2;
+@synthesize bluetoothEnabled;
+@synthesize btCont;
+@synthesize statusLabel;
 
 - (void)dealloc
 {
@@ -25,19 +33,34 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    Class BluetoothManager = objc_getClass("BluetoothManager");
+    btCont = [BluetoothManager sharedInstance];
+    
+    NSLog(@"Does BT Manager respond to sharedInstance? %@", btCont);
+    dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC);
+    dispatch_after(delay, dispatch_get_main_queue(), ^{ [self showBTStatus];});
 }
-*/
+
+-(void)showBTStatus {
+    BOOL btStatus = [btCont enabled];
+    NSLog(@"Current Bluetooth State: %d",btStatus);
+    [enableButton setTitle:btStatus ? @"Disable" :@"Enable" forState:UIControlStateNormal];
+    
+    [statusLabel setText:btStatus ? @"Enabled" :@"Disabled"];
+    [statusLabel setTextColor:btStatus ? [UIColor greenColor] : [UIColor redColor]];
+}
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    btCont = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -46,4 +69,13 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+-(IBAction)handleButtonPress:(id)sender {
+    
+    NSLog(@"Button Pressed");
+    [self.btCont setEnabled:![self.btCont enabled]];
+    dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC);
+    dispatch_after(delay, dispatch_get_main_queue(), ^{ [self showBTStatus];});
+
+    
+}
 @end
